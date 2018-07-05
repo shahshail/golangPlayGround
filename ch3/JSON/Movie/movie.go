b90fc37384fa090a33b2f6d9ec9cafdbab8ba314
Shail Shah <shahshail9898@gmail.com>
2018-07-05 01:53:33 -0400

    [Added] Movie Project Unmarshling (Decode JSON to GO DataStructure)

--- ch3/JSON/Movie/movie.go
+++ ch3/JSON/Movie/movie.go
@@ -21,10 +21,28 @@ func main() {
 	}
 
 	// Converting GO Datastructure in JOSON called Marshalling
+	// The main thing to note here is that only EXPORTED feilds can marshal so we have to use fields with Capital letter
+
 	data, err := json.MarshalIndent(movies, " ", "  ")
 	if err != nil {
 		log.Fatalf("Json Marshalling failed : %s", err)
 	}
-
 	fmt.Printf("%s\n", data)
+
+	//To decode JSON format to GO datastructure is called Unmarshaling.
+
+	var newMovies []struct {
+		Title  string
+		Year   int  `json:"released"`
+		Color  bool `json:"color,omitempty"`
+		Actors []string
+	}
+
+	newErr := json.Unmarshal(data, &newMovies)
+
+	if newErr != nil {
+		log.Fatalf("JSON unmarshalling failed: %s", newErr)
+	} else {
+		fmt.Println(newMovies) //[{Casablanca} {Scarface} {Milk}]
+	}
 }
