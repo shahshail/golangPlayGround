2332979c533dbe7b1459d69a6ee6f04c4fb1c81e
Shail Shah <shahshail9898@gmail.com>
2018-07-08 18:26:06 -0400

    [Added]Html-Parsing | FindLinks program

--- ch4/Recursion/findlinks.go
+++ ch4/Recursion/findlinks.go
@@ -16,4 +16,24 @@ func main() {
 	for _, link := range visit(nil, doc) {
 		fmt.Println(link)
 	}
+
+	// The visit function traverse an HTML node tree, extracts the link from the href attribute of eeach anchor element <a href = "">, appends the link to slice of strngs,
+	// and returns resultiung strings
+}
+
+func visit(links []string, n *html.Node) []string {
+
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c) // recursive call
+	}
+
+	return links
 }
