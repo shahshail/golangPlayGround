1ea95cd66b8485cb21b94c34e43540418dc3ddb3
Shail Shah <shahshail9898@gmail.com>
2018-07-08 18:35:30 -0400

    [Added] Html-Parsing outline

--- ch4/Recursion/Outline/outlines_html_parser.go
+++ ch4/Recursion/Outline/outlines_html_parser.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stdin, "outline: %v\n", err)
+		os.Exit(1)
+	}
+
+	outline(nil, doc)
+}
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data) // Push tag
+		fmt.Println(stack)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
 }
