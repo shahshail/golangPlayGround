0b304139582264df72a1f68a74e313247c1aeca2
Shail Shah <shahshail9898@gmail.com>
2018-09-09 21:16:08 -0400

    Cleanup

---
+++ ch4/methods/methods_demo.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type CAR struct {
+	name      string
+	car_type  string
+	top_speed int32
+}
+
+func main() {
+	fmt.Println("Method tutorial....")
+	my_car := CAR{"Rapid", "Sedan", 240}
+	fmt.Println("My care is", my_car)
+}
+
+fun(c CAR) change_top_speed(*car CAR){
+	
+} 
